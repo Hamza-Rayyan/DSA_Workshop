1085f1684fefea7530186dc46d9064d682b67b53
SYED HAMZAH RAYYAN <141110144+Hamza-Rayyan@users.noreply.github.com>
2023-10-27 22:31:35 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,7 @@
+def insertion_sort(seq):
+    for slice in range(len(seq)):
+        pos = slice
+        while pos > 0 and seq[pos] < seq[pos-1]:
+            (seq[pos],seq[pos-1]) = (seq[pos-1],seq[pos])
+            pos = pos-1
+    return(seq)
